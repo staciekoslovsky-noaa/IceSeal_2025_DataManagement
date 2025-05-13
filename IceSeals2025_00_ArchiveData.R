c51c68cf0f75d3bb5b5a6291868da29c39612815
staciekoslovsky-noaa <stacie.koslovsky@noaa.gov>
2025-05-13 10:28:27 -0700

    Update IceSeals2025_00_ArchiveData.R

--- IceSeals2025_00_ArchiveData.R
+++ IceSeals2025_00_ArchiveData.R
@@ -4,3 +4,5 @@ file.copy("D:\\fl108", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = FALSE, recu
 file.copy("D:\\fl204", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = TRUE, recursive = TRUE)
 
 file.copy("F:\\fl109", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = TRUE, recursive = TRUE)
+
+file.copy("D:\\fl207", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = TRUE, recursive = TRUE)
