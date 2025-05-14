efe5dda754018039d0c7de92f84426f37ae9d1a2
staciekoslovsky-noaa <stacie.koslovsky@noaa.gov>
2025-05-14 11:03:56 -0700

    Update IceSeals2025_00_ConvertFOV2KML.R
    Handle data where there's a large gap differently

--- IceSeals2025_00_ConvertFOV2KML.R
+++ IceSeals2025_00_ConvertFOV2KML.R
@@ -2,9 +2,9 @@
 # S. Koslovsky, 22 April 2025
 
 # Set variables
-flight_num <- 'fl127'
-flight_folder <- 'ice_seals_2025_fl127'
-date <- '0511'
+flight_num <- 'fl223'
+flight_folder <- 'ice_seals_2025_fl223'
+date <- '0513'
 
 # Load packages
 install_pkg <- function(x)
@@ -26,11 +26,15 @@ fov <- sf::st_read(shp) %>%
   st_centroid() %>% 
   arrange(image_file) %>%
   mutate(line_id = 1) %>%
-  select(image_file, effort, line_id)
+  mutate(last_time = lag(time)) %>%
+  mutate(time_diff = time - last_time)
 
 for (i in 2:nrow(fov)) {
-  fov$line_id[i] <- ifelse(fov$effort[i] == fov$effort[i-1], fov$line_id[i-1], fov$line_id[i-1] + 1)
+  fov$line_id[i] <- ifelse(fov$effort[i] == fov$effort[i-1] & fov$time_diff[i] < 30, fov$line_id[i-1], fov$line_id[i-1] + 1)
 }
+
+fov <- fov %>%
+  select(image_file, effort, line_id) 
 
 effort2line <- fov %>%
   st_drop_geometry() %>%
