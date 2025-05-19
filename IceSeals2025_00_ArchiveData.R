7752227fd06af69a8bf1d9ccb3fa0d42f13e5e10
staciekoslovsky-noaa <stacie.koslovsky@noaa.gov>
2025-05-19 13:32:32 -0700

    Update IceSeals2025_00_ArchiveData.R
    Added archive for Twin Otter NAS data through May 2025

--- IceSeals2025_00_ArchiveData.R
+++ IceSeals2025_00_ArchiveData.R
@@ -1,8 +1,35 @@
-file.copy("D:\\fl102", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = FALSE, recursive = TRUE)
-file.copy("D:\\fl108", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = FALSE, recursive = TRUE)
+file.copy("D:\\fl102", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = FALSE, recursive = TRUE) # done
+file.copy("D:\\fl108", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = FALSE, recursive = TRUE) # done
 
-file.copy("D:\\fl204", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = TRUE, recursive = TRUE)
+file.copy("D:\\fl204", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = TRUE, recursive = TRUE) # done
 
-file.copy("F:\\fl109", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = TRUE, recursive = TRUE)
+file.copy("F:\\fl109", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = TRUE, recursive = TRUE) # done
 
-file.copy("D:\\fl207", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = TRUE, recursive = TRUE)
+file.copy("D:\\fl207", "L:\\Surveys_IceSeals_2025_TEMP", overwrite = TRUE, recursive = TRUE) # done
+
+file.copy("L:\\Surveys_IceSeals_2025_TEMP", "M:\\", overwrite = TRUE, recursive = TRUE) # in progress 5/19/2025
+
+setwd("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025")
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl101", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE) # in progress 5/19/2025
+
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl103", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl104", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl105", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl106", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl107", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl110", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl114", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl115", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl116", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl119", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl120", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl121", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl122", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl123", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl125", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl126", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
+file.copy("\\\\4.0.0.1\\kamera\\flight_data\\ice_seals_2025/fl127", "M:\\Surveys_IceSeals_2025_TEMP\\", overwrite = TRUE, recursive = TRUE)
